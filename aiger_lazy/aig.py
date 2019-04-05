3f977692fa35a7820df6263501e446f11843d1b2
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-04-04 18:12:49 -0700

    initial ideas for aiger-lazy

---
+++ aiger_lazy/aig.py
@@ -0,0 +1,105 @@
+from enum import Enum
+from typing import Mapping, Tuple, FrozenSet, Union
+from functools import partial
+
+import aiger
+import attr
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class SeqComp:
+    left: "AIG"
+    right: "AIG"
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class ParComp:
+    left: "AIG"
+    right: "AIG"
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class Wire:
+    input: str
+    output: str
+    latch: Union[str, None] = None
+    initial: bool = False
+    keep_output: bool = True
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class Feedback:
+    data: "AIG"
+    wires: Tuple[Wire]
+
+
+@attr.s(frozen=True, auto_attribs=True, repr=False)
+class LazyAIG:
+    data: Union[ParComp, SeqComp, Feedback, aiger.AIG]
+    inputs: FrozenSet[str] = frozenset()
+    outputs: FrozenSet[str] = frozenset()
+    latches: FrozenSet[str]  = frozenset()
+
+    def __repr__(self):
+        return repr(self.flatten())
+
+    @property
+    def aig(self):
+        return self
+
+    def __getattr__(self, attr):
+        # TODO:
+        return partial(getattr(aiger.AIG, attr), self=self)
+
+    def __rshift__(self, other):
+        interface = self.outputs & other.inputs
+        assert not (self.outputs - interface) & other.outputs
+        assert not self.latches & other.latches
+        return LazyAIG(
+            data=SeqComp(left=self, right=other),
+            inputs=self.inputs | (other.inputs - interface),
+            outputs=(self.outputs - interface) | other.outputs,
+            latches=self.latches | other.latches
+        )
+
+    def __or__(self, other):
+        assert not aig1.outputs & aig2.outputs
+        assert not aig1.latches & aig2.latches
+        return LazyAIG(
+            data=ParComp(left=self, right=other),
+            inputs=self.inputs | other.inputs,
+            outputs=self.outputs | other.outputs,
+            latches=self.latches | other.latches
+        )
+
+    def flatten(self):
+        # TODO: implement non-naive flattening
+        data = self.data
+        if isinstance(data, ParComp):
+            return data.left.flatten() | data.right.flatten()
+
+        elif isinstance(data, Feedback):
+            # TODO: update after aiger feedback api update.
+
+            return data.flatten().feedback(
+                inputs=[x.input for x in data.wires],
+                outputs=[x.output for x in data.wires],
+                initials=[x.initial for x in data.wires],
+                latches=[x.latch for x in data.wires],
+                keep_output=data.wires[0].keep_output
+            )
+
+        elif isinstance(data, SeqComp):
+            return data.left.flatten() >> data.right.flatten()
+
+        else:
+            return self.data
+
+
+def lazify(circ):
+    return LazyAIG(
+        data=circ, 
+        inputs=circ.inputs,
+        outputs=circ.outputs,
+        latches=circ.latches
+    )
